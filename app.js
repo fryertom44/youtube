9a8fed83add606303b4b3b45ffc9143755879496
Tom Fryer <fryertom@hotmail.com>
2019-08-16 16:26:12 +0100

    Add node-fetch module

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var nodeFetch = require('node-fetch');
 require('dotenv').config();
 
 var users = require('./routes/users');
