9b7b8dffcddfbf030b22206cae7083ac080cb905
Tom Fryer <fryertom@hotmail.com>
2019-08-20 13:27:13 +0100

    Add model validations

--- models/channel.js
+++ models/channel.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Channel = sequelize.define('Channel', {
-    channel_name: DataTypes.STRING
+    channel_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      }
+    }
   }, {timestamps: false});
+
   Channel.associate = function(models) {
     // associations can be defined here
   };
--- models/video.js
+++ models/video.js
@@ -1,8 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Video = sequelize.define('Video', {
-    title: DataTypes.STRING,
-    date: DataTypes.DATE
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    date: {
+      type: DataTypes.DATE,
+      validate: {
+        notEmpty: true,
+        isDate: true,
+      }
+    },
   }, {timestamps: false});
   Video.associate = function(models) {
     // associations can be defined here
